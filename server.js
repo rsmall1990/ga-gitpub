989fbbe4f8bf577ae2d8f4e36c987184bca9433c
Ryan Small <ryan.small@feapc.com>
2021-11-20 16:27:52 -0500

    wip: create Show route

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // require dependencies
 require("dotenv").config();
 const express = require("express");
-const drinks = require("./models/drinks.js") // <-- must use relative file path to require
+const drinks = require("./models/drinks.js"); // <-- must use relative file path to require
 
 // initialize app
 const app = express();
@@ -11,30 +11,32 @@ const port = process.env.PORT; // configure port
 
 // handle data
 
-
 // mount middleware
 
 // Always think INDUCES
 // Welcome Route
 app.get("/", (req, res) => {
-    res.send("Welcome to the Gitpub App!")
+  res.send("Welcome to the Gitpub App!");
 });
 
 // Index
 app.get("/drinks", (req, res) => {
-    res.render("./drinks_index.ejs", {
-        drinks
-    });
-})
+  res.render("./drinks_index.ejs", {
+    drinks,
+  });
+});
 
 // New
 // Delete
 // update
 // Create
 // edit
+
 // Show
-app.get("/drinks/:id", (req,res) => {
-    res.send(req.params.id);
+app.get("/drinks/:id", (req, res) => {
+  res.render("./drinks_show.ejs", {
+    drink: drinks[req.params.id],
+  });
 });
 
 // tell app to listen
